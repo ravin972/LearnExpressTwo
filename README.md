48f32a21187f8b85d42903960afd841a77a4b435
Ravinder Pandey <pandeyravinder078@gmail.com>
2023-11-09 21:03:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Learn Express Two :
+
+*Again we try via window in linux terminal its given me error*
+
+```If you can run in the linux : fix this ```
+
+"I wanna install ```express-generator``` it's gives me error"
---
+++ script.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+  console.log("Server running!");
+});
+
+app.listen(3000);
